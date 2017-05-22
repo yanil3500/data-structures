338d27ad7a83aa2c86c56e2a76700fc558e771e2
yanil3500 <yanil3500@gmail.com>
2017-05-22 08:51:00 -0700

    creates the setup.py; configures settings in setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+setup for data-structures
+"""
+from setuptools import setup
+
+DEPENDENCIES = ['pytest', 'pytest-cov', 'ipython']
+MODULES = [
+    ]
+EXTRA_PACKAGES = {
+    'test': ['tox']
+}
+CONSOLE_SCRIPTS = {
+    'console_scripts': [
+    ]
+}
+setup(
+    name="data-structures",
+    description="""A module that contains code samples for a number of classic data structures implemented in Python.""",
+    version='0.1',
+    author='Elyanil Castro',
+    author_email='yanil3500@gmail.com',
+    license='MIT',
+    package_dir={'': 'src'},
+    # insert the names of pymodule into array
+    py_modules=MODULES,
+    install_requires=DEPENDENCIES,
+    extras_require=EXTRA_PACKAGES,
+    # console scripts allow for custom commands
+    entry_points=CONSOLE_SCRIPTS
+
+
+)
