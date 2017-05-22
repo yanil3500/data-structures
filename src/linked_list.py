6df25d7017dbcdd55630601d5e32cb3db15f408e
Miguel Pena <miguelp1986@gmail.com>
2017-05-22 13:40:33 -0700

    started linked_list

--- src/linked_list.py
+++ src/linked_list.py
@@ -12,13 +12,27 @@ class Node:
         self.next = None
 
 
-class Singly_Linked_List:
+class LinkedList:
     """
     singly linked list
     """
     head = None
     length = 0
+    def __init__(self,values_list=[]):
+        if len(values_list) > 0:
+            """where we create Nodes"""
 
+    def push(self, value):
+        if head == None:
+            temp_node = Node(value)
+            head = temp_node
+            self.length += 1
+
+        else:
+            temp_node = Node(value)
+            temp_node.next = self.head
+            self.head = temp_node
+            self.length += 1
 
 def main():
     """
