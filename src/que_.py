7a52c1cffc6e1fa1fb26b345b64e07e10c42f981
yanil3500 <yanil3500@gmail.com>
2017-05-25 17:30:11 -0700

    adds enqueue and dequeu

--- src/que_.py
+++ src/que_.py
@@ -7,7 +7,11 @@ def main():
     """
     the main function
     """
-    pass
+    a_queue = Queue()
+
+    for value in 'cash-rules-everthing-around-me':
+        a_queue.enqueue(value)
+
 
 class Node:
     def __init__(self, value):
@@ -16,6 +20,7 @@ class Node:
         """
         self.value = value
         self.next = None
+
 
 class Queue:
     def __init__(self):
@@ -26,18 +31,41 @@ class Queue:
         self._length = 0
 
     def enqueue(self, value):
-            if not self._front:
-                self._front = self._rear = Node(value)
-                self._length += 1
-            else:
-                self._rear.next = Node(value)
-                self._rear = self._rear.next
-                self._length += 1
+        """
+        adds elements to the queue
+        """
+        if not self._front:
+            self._front = self._rear = Node(value)
+            self._length += 1
+        else:
+            self._rear.next = Node(value)
+            self._rear = self._rear.next
+            self._length += 1
 
-    
-
-
-
+    def dequeue(self):
+        """
+        removes the elements in the order in which they were added
+        """
+        try:
+            if self.length > 0:
+                if self._front is self._rear:
+                    new_item = self._front.value
+                    self._front = self._rear = None
+                    new_item.next = None
+                    self._length -= 1
+                    return new_item.value
+                else:
+                    new_item = self._front
+                    self._front = self._front.next
+                    new_item.next = None
+                    self._length -= 1
+                    return new_item.value
+        except:
+            raise IndexError("Cannot remove elements from empty queue.")
+        
+    def __str__(self):
+        a_string = ''
+        current = self._front
 
 
 if __name__ == "__main__":
