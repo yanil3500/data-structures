785c1baa19373d458a7cb0515321e8735ab32e11
yanil3500 <yanil3500@gmail.com>
2017-06-01 22:49:30 -0700

    adds priorityq.py as one of the modules in our data-structure package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup for data-structures
 from setuptools import setup
 
 DEPENDENCIES = ['pytest', 'pytest-cov', 'ipython']
-MODULES = ['linked_list', 'stack', 'doubly_linked_list', 'deque', 'binheap']
+MODULES = ['linked_list', 'stack', 'doubly_linked_list', 'deque', 'binheap', 'priorityq']
 EXTRA_PACKAGES = {
     'test': ['tox']
 }
