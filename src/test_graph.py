f1e7939e5b4e1417d9de9cd1be36b734a663fd68
yanil3500 <yanil3500@gmail.com>
2017-06-07 14:46:12 -0700

    adds parameters for depth-first-traversal

--- src/test_graph.py
+++ src/test_graph.py
@@ -5,12 +5,7 @@ Tests for Graph
 import pytest
 
 PARAMETERS_FOR_TEST_DEPTH_FIRST_TRAVERSAL = [
-    ('A', 'B'),
-    ('A' 'C'),
-    ('B', 'H'),
-    ('B', 'I'),
-    ('H', 'G'),
-    (),
+    [('A', 'B'), ('A' 'C'), ('B', 'H'), ('B', 'I'), ('H', 'G'), ('C', 'F'), ('C', 'G')]
 ]
 
 
